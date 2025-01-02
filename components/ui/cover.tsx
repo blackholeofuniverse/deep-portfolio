ac569863e8afed15f511609c3d2a23176c6a79c7
Samrat <asustufgaming1050@gmail.com>
2025-01-02 22:46:25 +0530

    fix issues

--- components/ui/cover.tsx
+++ components/ui/cover.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import React, { useEffect, useId, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
